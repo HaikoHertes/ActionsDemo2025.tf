a437cd0757aa386cc6c2c7cef0183959770f98ce
Haiko Hertes <github@hertes.net>
2025-02-21 22:34:37 +0100

    Removed required_version

--- Terraform/terraform.tf
+++ Terraform/terraform.tf
@@ -8,8 +8,6 @@ terraform {
     key                  = "github.terraform.tfstate"
   }
 
-  required_version = ">=0.12"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
